28da7286b0bfe6bf366f72b2d9bfaad121b6cba6
masashitamayama <okita-souji.7-7-3@outlook.jp>
2024-12-10 05:56:59 +0900

    #3 ユーザー登録フォームの作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,7 @@
         <title>Ray mail system</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <style>
-            .bg-dark {
-                background-color: #4682B4 !important;
-            }
-        </style>
+        <link rel="stylesheet" href="{{ asset('css/custom.css') }}"> <!-- custom.cssを読み込む -->
     </head>
     <body>
         @include('commons.header')
