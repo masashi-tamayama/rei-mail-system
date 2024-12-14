2ecfbe94ae63e7bb96ccde441cacb3e1b385dc37
masashitamayama <okita-souji.7-7-3@outlook.jp>
2024-12-14 20:00:57 +0900

    ＃13 メールリスト管理画面の作成

--- resources/views/mail-list/index.blade.php
+++ resources/views/mail-list/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <h1>メールリスト管理</h1>
-    @include('includes.success_messages')
+    @include('commons.success_messages')
     <table class="table">
         <thead>
             <tr>
